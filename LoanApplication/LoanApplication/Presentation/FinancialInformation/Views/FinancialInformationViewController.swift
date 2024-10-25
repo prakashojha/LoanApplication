f8cc1f7abe48126d172c93b8d6ace0ad5c43adae
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-25 19:59:54 +1300

    contraints + hidekeyboard logic added

--- LoanApplication/LoanApplication/Presentation/FinancialInformation/Views/FinancialInformationViewController.swift
+++ LoanApplication/LoanApplication/Presentation/FinancialInformation/Views/FinancialInformationViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class FinancialInformationViewController: UIViewController {
+class FinancialInformationViewController: UIViewController, HideKeyboardProtocol {
+    
+    let viewModel: FinancialInformationViewModel = FinancialInformationViewModel()
 
+    private lazy var financialInformationView: FinancialInformationView = {
+        return FinancialInformationView(viewModel: viewModel)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .yellow
         navigationItem.title = "Financial Information"
-       
+        addView()
+        setConstraints()
+        hideKeyboardWhenTappedAround()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,6 +38,22 @@ class FinancialInformationViewController: UIViewController {
         navigationItem.standardAppearance = navigationBarAppearance
         navigationItem.compactAppearance = navigationBarAppearance
         navigationItem.scrollEdgeAppearance = navigationBarAppearance
+    }
+    
+    @objc func dismissKeyboardView() {
+        view.endEditing(true)
+    }
+    
+    func addView() {
+        self.view.addSubview(financialInformationView)
+    }
+    
+    func setConstraints() {
+        financialInformationView.translatesAutoresizingMaskIntoConstraints = false
+        financialInformationView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        financialInformationView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        financialInformationView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -80).isActive = true
+        
     }
 
 }
