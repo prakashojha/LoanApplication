67f38cbc8c75cbb8bb7ac005c83196c5869753f1
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-25 14:39:18 +1300

    setup navigation bar

---
+++ LoanApplication/LoanApplication/Presentation/PersonLInformation/View/LoanApplicationViewController.swift
@@ -0,0 +1,70 @@
+//
+//  LoanApplicationViewController.swift
+//  LoanApplication
+//
+//  Created by Saumya Prakash on 24/10/24.
+//
+
+import UIKit
+
+class LoanApplicationViewController: UIViewController, HideKeyboardProtocol, ShowAlertProtocol {
+    
+    let viewModel: PersonalInformationViewModel
+    
+    init(viewModel: PersonalInformationViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var personalInformationView: PersonalInformationView = {
+        viewModel.alertMessage = { (message, completion) in
+            self.showAlert(with: message, completion: completion)
+        }
+        return PersonalInformationView(viewModel: viewModel)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        navigationItem.title = "Personal Information"
+        addView()
+        setConstraints()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setUpNavigationBar()
+    }
+    
+    func setUpNavigationBar() {
+
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = .systemBlue
+
+        navigationItem.standardAppearance = navigationBarAppearance
+        navigationItem.compactAppearance = navigationBarAppearance
+        navigationItem.scrollEdgeAppearance = navigationBarAppearance
+    }
+    
+    @objc func dismissKeyboardView() {
+        view.endEditing(true)
+    }
+    
+    func addView() {
+        self.view.addSubview(personalInformationView)
+    }
+    
+    func setConstraints() {
+        personalInformationView.translatesAutoresizingMaskIntoConstraints = false
+        personalInformationView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        personalInformationView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        personalInformationView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -80).isActive = true
+        
+    }
+}
+
