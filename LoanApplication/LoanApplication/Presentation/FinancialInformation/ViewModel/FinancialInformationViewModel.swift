c3481e0860047b286971e077e541f7ab9c0cb80b
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-25 19:57:45 +1300

    create second scene view model

---
+++ LoanApplication/LoanApplication/Presentation/FinancialInformation/ViewModel/FinancialInformationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FinancialInformationViewModel.swift
+//  LoanApplication
+//
+//  Created by Saumya Prakash on 25/10/24.
+//
+
+import Foundation
+
+class FinancialInformationViewModel {
+    let gapBetween: CGFloat = 15.0
+    let viewHeight: CGFloat = 60.0
+    let gapLeft: CGFloat = 12.0
+    let gapRight: CGFloat = 12.0
+}
