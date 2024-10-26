0cc009b640d9d3ab80adc939765a94a8f4acdab6
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-27 01:08:41 +1300

    add field for text

--- LoanApplication/LoanApplication/Shared/View/LAUITextField.swift
+++ LoanApplication/LoanApplication/Shared/View/LAUITextField.swift
@@ -12,17 +12,20 @@ class LAUITextField: UITextField {
     
     let edgeInsets: UIEdgeInsets
     let placeHolderText: String
+    let textFieldText: String?
     let textFieldFontSize: CGFloat = 18
     
-    init(edgeInsets: UIEdgeInsets = .init(top: 0, left: 12, bottom: 0, right: 12), _ placeHolderText: String) {
+    init(edgeInsets: UIEdgeInsets = .init(top: 0, left: 12, bottom: 0, right: 12), _ placeHolderText: String, _ text: String? = nil) {
         self.edgeInsets = edgeInsets
         self.placeHolderText = placeHolderText
+        self.textFieldText = text
         super.init(frame: .zero)
         
         self.layer.cornerRadius = 8
         self.layer.borderWidth = 1.5
         self.layer.borderColor = UIColor.lightGray.cgColor
         self.placeholder = placeHolderText
+        self.text = textFieldText
         self.font = UIFont.systemFont(ofSize: textFieldFontSize, weight: .medium)
     }
     
