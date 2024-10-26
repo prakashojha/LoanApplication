312159ced41073e98da52098dc341398dcec363a
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-27 01:12:08 +1300

    handle validation and input formatting

--- LoanApplication/LoanApplication/Presentation/FinancialInformation/Views/FinancialInformationView.swift
+++ LoanApplication/LoanApplication/Presentation/FinancialInformation/Views/FinancialInformationView.swift
@@ -12,27 +12,47 @@ class FinancialInformationView: UIView {
     
     private let viewModel: FinancialInformationViewModel
     
-    private lazy var annualIncome: LAUITextField = {
-        let view: LAUITextField = LAUITextField("Annual Income")
+    private lazy var annualIncome: LAUIInputField = {
+        let view: LAUIInputField = LAUIInputField(viewModel.annualIncome, labelText: "Annual Income")
+        view.textField.tag = 1
+        view.textField.delegate = self
         return view
     }()
     
-    private lazy var desiredLoanAmount: LAUITextField = {
-        let view: LAUITextField = LAUITextField("Desired Loan Amount")
-        view.autocapitalizationType = .none
+    private lazy var desiredLoanAmount: LAUIInputField = {
+        let view: LAUIInputField = LAUIInputField(viewModel.desiredLoanAmount, labelText: "Loan Amount (50% of annual income)")
+        view.textField.autocapitalizationType = .none
+        view.textField.tag = 2
+        view.textField.delegate = self
         return view
     }()
     
-    private lazy var irdNumber: LAUITextField = {
-        let view: LAUITextField = LAUITextField("IRD Number")
-        view.autocapitalizationType = .none
+    private lazy var irdNumber: LAUIInputField = {
+        let view: LAUIInputField = LAUIInputField(viewModel.irdNumber, labelText: "IRD Number")
+        view.textField.autocapitalizationType = .none
+        view.textField.tag = 3
+        view.textField.delegate = self
         return view
+    }()
+    
+    private lazy var previousButton: LAUIButton = {
+        let button = LAUIButton(configuration: .navigationCustomButton(title: "Previous"))
+        button.addTarget(self, action: #selector(onPreviousButtonPressed), for: .touchUpInside)
+        return button
     }()
     
     private lazy var nextButton: LAUIButton = {
-        let button = LAUIButton(configuration: .financialInformationCustomButton)
+        let button = LAUIButton(configuration: .navigationCustomButton(title: "Next"))
         button.addTarget(self, action: #selector(onNextButtonPressed), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var navigationButtonView: UIStackView = {
+        let view: UIStackView = UIStackView()
+        view.addArrangedSubview(previousButton)
+        view.addArrangedSubview(nextButton)
+        view.spacing = 10
+        return view
     }()
     
     init(viewModel: FinancialInformationViewModel) {
@@ -48,21 +68,29 @@ class FinancialInformationView: UIView {
     }
     
     @objc func onNextButtonPressed() {
-        
+        viewModel.onNextButtonPressed(
+            annualIncome.textField.text,
+            desiredLoanAmount.textField.text,
+            irdNumber: irdNumber.textField.text
+        )
+    }
+    
+    @objc func onPreviousButtonPressed() {
+        viewModel.onPreviousButtonPressed()
     }
     
     func setupView() {
         addSubview(annualIncome)
         addSubview(desiredLoanAmount)
         addSubview(irdNumber)
-        addSubview(nextButton)
+        addSubview(navigationButtonView)
     }
     
     func setupConstraints() {
         annualIncome.translatesAutoresizingMaskIntoConstraints = false
         desiredLoanAmount.translatesAutoresizingMaskIntoConstraints = false
         irdNumber.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        navigationButtonView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             annualIncome.topAnchor.constraint(equalTo: self.topAnchor),
@@ -80,15 +108,27 @@ class FinancialInformationView: UIView {
             irdNumber.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -viewModel.gapRight),
             irdNumber.heightAnchor.constraint(equalToConstant: viewModel.viewHeight),
             
-            nextButton.topAnchor.constraint(equalTo: irdNumber.bottomAnchor, constant: 50),
-            nextButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32),
-            nextButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32),
-            nextButton.heightAnchor.constraint(equalToConstant: 56),
-            nextButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            nextButton.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        
+            navigationButtonView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -30),
+            navigationButtonView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32),
+            navigationButtonView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32),
+            navigationButtonView.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
-    
-
 }
+
+extension FinancialInformationView: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Get the updated text after the replacement
+        var updatedText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
+        guard viewModel.containValidAmount(updatedText: updatedText, replacementString: string) else { return false }
+        
+        // Directly update the text field to allow typing of decimal part
+        textField.text = updatedText
+        if let formattedText = viewModel.getFormattedText(updatedText, string, textField.tag) {
+            textField.text = formattedText
+        }
+        return false
+    }
+}
+
