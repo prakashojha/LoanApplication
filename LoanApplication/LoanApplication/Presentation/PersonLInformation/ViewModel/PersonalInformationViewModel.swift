1bc638abcd16f1940c0b1acbabef80b24e4f6662
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-27 01:17:24 +1300

    load next screen logic

--- LoanApplication/LoanApplication/Presentation/PersonLInformation/ViewModel/PersonalInformationViewModel.swift
+++ LoanApplication/LoanApplication/Presentation/PersonLInformation/ViewModel/PersonalInformationViewModel.swift
@@ -11,13 +11,12 @@ final class PersonalInformationViewModel {
     typealias Completion = () -> Void
     typealias AlertType = ( _: String, _: Completion? ) -> Void
    
-    private var model: PersonalInformationModel
+    private var model: LoanApplicationModel.PersonalInformationModel
     
-    var delegate: PersonalInformationDelegate?
     var alertMessage: AlertType?
     var coordinator: AppCoordinator?
     
-    init(model: PersonalInformationModel, coordinator: AppCoordinator) {
+    init(model: LoanApplicationModel.PersonalInformationModel, coordinator: AppCoordinator) {
         self.model = model
         self.coordinator = coordinator
     }
@@ -25,12 +24,12 @@ final class PersonalInformationViewModel {
     let genders: [String] = ["Male", "Female"]
     let gapBetween: CGFloat = 15.0
     let viewHeight: CGFloat = 78.0
-    let gapLeft: CGFloat = 12.0
-    let gapRight: CGFloat = 12.0
+    let gapLeft: CGFloat = 0.0
+    let gapRight: CGFloat = 0.0
     
     var firstName: String {
-        get { model.firstName }
-        set { model.firstName = newValue}
+        get { model.fullName }
+        set { model.fullName = newValue}
     }
     
     var email: String {
@@ -53,31 +52,9 @@ final class PersonalInformationViewModel {
         set { model.address = newValue }
     }
     
-    private func trimSpace(from text: String?) -> String? {
-        guard let text = text else { return nil }
-        return text.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-    
-    private func isValidString(_ checkString: String?) -> String? {
-        guard let checkString = trimSpace(from: checkString), !checkString.isEmpty else { return nil }
-        return checkString
-    }
-    
-    private  func isValidPhone(phoneNumber: String) -> Bool {
-        let phoneRegex = "^[0-9+]{0,1}+[0-9]{5,16}$"
-        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
-        return phoneTest.evaluate(with: phoneNumber)
-    }
-    
-    private func isValidEmailAddress(email: String?) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: email)
-    }
-    
     private func checkFirstNameIsValid(firstName: String?) -> String? {
-        guard let trimmedFirstName = isValidString(firstName) else {
-            alertMessage?("First Name cannot be empty"){}
+        guard let trimmedFirstName = Helper.isValidString(firstName) else {
+            alertMessage?("Full Name cannot be empty"){}
             return nil
         }
         
@@ -85,12 +62,12 @@ final class PersonalInformationViewModel {
     }
     
     private func checkEmailIsValid(email: String?) -> String? {
-        guard let trimmedEmail = isValidString(email) else {
-            alertMessage?("Email cannot be empty"){}
+        guard let trimmedEmail = Helper.isValidString(email) else {
+            alertMessage?( "Email cannot be empty"){}
             return nil
         }
         
-        guard isValidEmailAddress(email: trimmedEmail) else {
+        guard Helper.isValidEmailAddress(email: trimmedEmail) else {
             alertMessage?("Invalid Email. Enter valid email address"){}
             return nil
         }
@@ -99,12 +76,12 @@ final class PersonalInformationViewModel {
     }
     
     private func checkPhoneNumberIsValid(phoneNumber: String?) -> String? {
-        guard let trimmedPhoneNumber = isValidString(phoneNumber) else {
+        guard let trimmedPhoneNumber = Helper.isValidString(phoneNumber) else {
             alertMessage?("Phone Number cannot be empty"){}
             return nil
         }
         
-        guard isValidPhone(phoneNumber: trimmedPhoneNumber) else {
+        guard Helper.isValidPhone(phoneNumber: trimmedPhoneNumber) else {
             alertMessage?("Invalid phone number. Enter valid number"){}
             return nil
         }
@@ -121,13 +98,21 @@ final class PersonalInformationViewModel {
     }
     
     func onNextButtonPressed(_ firstName: String?, _ email: String?, _ phoneNumber: String?, _ gender: String?, _ address: String?) {
-        
+
         guard let firstName = checkFirstNameIsValid(firstName: firstName) else { return }
         guard let email = checkEmailIsValid(email: email) else { return }
         guard let phoneNumber = checkPhoneNumberIsValid(phoneNumber: phoneNumber) else { return }
-        
+
         self.updateModel(firstName: firstName, email: email, phoneNumber: phoneNumber, gender: gender!, address: address ?? "Not provided")
-        
-        self.coordinator?.loadNextScreen(screen: Screens.PersonalInformationScreen)
+
+        loadNextScreen()
+    }
+    
+    func loadNextScreen() {
+        self.coordinator?.loadNextScreen(fromScreen: .PersonalInformationScreen(model: model))
+    }
+    
+    func loadPreviousScreen() {
+        //self.coordinator?.loadPreviousScreen(screen: Screen.PersonalInformationScreen, loanData: model)
     }
 }
