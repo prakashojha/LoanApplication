f5ae32c77efa607160c9ee0b99d747c2b064760f
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-27 17:38:36 +1300

    core data related changes

---
+++ LoanApplication/LoanApplication/Data/CoreData/LoanApplicationCDModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  LoanApplicationCDModel+CoreDataClass.swift
+//  LoanApplication
+//
+//  Created by Saumya Prakash on 27/10/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(LoanApplicationCDModel)
+public class LoanApplicationCDModel: NSManagedObject {
+
+}
---
+++ LoanApplication/LoanApplication/Data/CoreData/LoanApplicationCDModel+CoreDataProperties.swift
@@ -0,0 +1,33 @@
+//
+//  LoanApplicationCDModel+CoreDataProperties.swift
+//  LoanApplication
+//
+//  Created by Saumya Prakash on 27/10/24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension LoanApplicationCDModel {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LoanApplicationCDModel> {
+        return NSFetchRequest<LoanApplicationCDModel>(entityName: "LoanApplicationCDModel")
+    }
+
+    @NSManaged public var address: String?
+    @NSManaged public var annualIncome: String?
+    @NSManaged public var desiredLoanAmount: String?
+    @NSManaged public var email: String?
+    @NSManaged public var fullName: String?
+    @NSManaged public var gender: String?
+    @NSManaged public var irdNumber: String?
+    @NSManaged public var phoneNumber: String?
+    @NSManaged public var dateSubmitted: Date?
+
+}
+
+extension LoanApplicationCDModel : Identifiable {
+
+}
