41bf0dc9e7cfe87600040f8447db6272de7ba405
Bindu Ojha <prakashojha2005@gmail.com>
2024-10-29 12:30:07 +1300

    digit count restriction only for ird

--- LoanApplication/LoanApplication/Presentation/FinancialInformation/Views/FinancialInformationView.swift
+++ LoanApplication/LoanApplication/Presentation/FinancialInformation/Views/FinancialInformationView.swift
@@ -142,9 +142,11 @@ extension FinancialInformationView: UITextFieldDelegate {
         guard let currentText = textField.text else { return true }
         
         // Check the maximum length of 11 characters
-        let newLength = currentText.count + string.count - range.length
-        if newLength > 11 {
-            return false
+        if textField.tag == 3{
+            let newLength = currentText.count + string.count - range.length
+            if newLength > 11 {
+                return false
+            }
         }
         // Get the updated text after the replacement
         let updatedText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? ""
